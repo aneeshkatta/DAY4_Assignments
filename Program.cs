2373a149368e6abec7805a36494f436bd7c482f3
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-12 13:57:32 +0530

    Add files via upload
    added uc2 snakeladder

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase2SnakeLadder
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            UC2_SnakeLadder.RollDice();
+        }
+    }
+}
---
+++ UC2_SnakeLadder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase2SnakeLadder
+{
+   public class UC2_SnakeLadder
+    {
+        public static void RollDice()
+
+        {
+            int START_POSITION = 0;
+            int END_POSITION = 100;
+            int dieRoll;
+
+            Random random = new Random();
+            dieRoll = random.Next(1, 7);
+            Console.WriteLine("Number on die is: " + dieRoll);
+        }
+    }
+}
