6f8c48c36a2b9abfaa97314e4b850e2f8112976d
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-12 13:56:37 +0530

    Add files via upload
    added uc1 snakeladder

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderProblem
+{
+    public class Program
+    {
+     public static void Main()
+        {
+            UC_01.usercaseone();
+        }
+    }
+}
---
+++ UC_01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderProblem
+{
+    public class UC_01
+    {
+        public static void usercaseone()
+        {
+
+            int START_POSITION = 0;
+           
+                Console.WriteLine("Welcome to Snake And Ladder Game");
+                Console.WriteLine("Player will Start at Zero Position");
+            
+        } 
+    }
+}
