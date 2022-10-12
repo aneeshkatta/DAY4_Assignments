6a11017fed006625144234a26ae5c04a2279c161
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-12 14:50:36 +0530

    Add files via upload
    added uc04 snakeladder

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCase04_SnakeLadder
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Uc04_snakeladder.usercase04();
+        }
+    }
+}
---
+++ Uc04_snakeladder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCase04_SnakeLadder
+{
+    class Uc04_snakeladder
+    {
+        public static void usercase04()
+        {
+            int START_POSITION = 0;
+            int WIN_POSITION = 100;
+            int CURRENTPOSITION = 0;
+            int dierollcount = 0;
+            int dieRoll;
+            while (CURRENTPOSITION < WIN_POSITION)
+            {
+                dierollcount++;
+                Random random1 = new Random();
+                int checkOptions = random1.Next(0, 3);
+                switch (checkOptions)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("There will be no play. Player will be at same position");
+                        }
+                        break;
+
+                    case 1:
+                        {
+
+                            Console.WriteLine("Player will move forward");
+                            Random random = new Random();
+                            dieRoll = random.Next(1, 7);
+                            if (CURRENTPOSITION < 100)
+                            {
+                                CURRENTPOSITION += dieRoll;
+                            }
+                           
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Player is bitten by a snake");
+                            Random random = new Random();
+                            dieRoll = random.Next(1, 7);
+                            if (CURRENTPOSITION > 0)
+                            {
+                                CURRENTPOSITION -= dieRoll;
+
+                            }
+                            else if (CURRENTPOSITION <= 0)
+                            {
+                                CURRENTPOSITION = START_POSITION;
+                            }
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("invalid");
+                        break;
+                }
+                Console.WriteLine("CURRENTPOSITION is " + CURRENTPOSITION);
+
+            }
+            
+        
+         }
+    }
+}
